ac00c9571b1906947ba07a01de8ad94d16346a15
hany <hany.alsamman@gmail.com>
2013-12-20 10:50:22 +0000

    enable developer options by default
    Change-Id: I4601c482feb89eda5989ccce40fa3b2bf90dde83

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -564,9 +564,7 @@ public class Settings extends PreferenceActivity
     }
 
     private void updateHeaderList(List<Header> target) {
-        final boolean showDev = mDevelopmentPreferences.getBoolean(
-                DevelopmentSettings.PREF_SHOW,
-                android.os.Build.TYPE.equals("eng"));
+	final boolean showDev = (UserHandle.myUserId() == UserHandle.USER_OWNER);
         int i = 0;
 
         final UserManager um = (UserManager) getSystemService(Context.USER_SERVICE);
