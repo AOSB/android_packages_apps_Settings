95f2a541d59c988c82cf14655a3be6578802442a
hany <hany.alsamman@gmail.com>
2013-12-20 08:41:52 +0000

    FULL ActiveDisplay Settings
    Pac : Pimp up ActiveDisplay
    f7a3e64fa3 Browse code
    Der-Schubi authored
    
    OmniGears: Fix pimp up ActiveDisplay …
    9852cc52b6 Browse code
    micku7zu authored
    
    Change-Id: I51b2943829564910a3955d7f56fa7f872741e637

--- src/com/android/settings/crdroid/ActiveDisplaySettings.java
+++ src/com/android/settings/crdroid/ActiveDisplaySettings.java
@@ -52,6 +52,9 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_SHOW_DATE = "ad_show_date";
     private static final String KEY_SHOW_AMPM = "ad_show_ampm";
     private static final String KEY_BRIGHTNESS = "ad_brightness";
+    private static final String KEY_TIMEOUT = "ad_timeout";
+    private static final String KEY_THRESHOLD = "ad_threshold";
+    private static final String KEY_TURNOFF_MODE = "ad_turnoff_mode";
 
     private SwitchPreference mEnabledPref;
     private CheckBoxPreference mShowTextPref;
@@ -63,8 +66,12 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mSunlightModePref;
     private ListPreference mRedisplayPref;
     private SeekBarPreferenceChOS mBrightnessLevel;
+    private ListPreference mDisplayTimeout;
+    private ListPreference mProximityThreshold;
+    private CheckBoxPreference mTurnOffModePref;
     private AppMultiSelectListPreference mExcludedAppsPref;
 
+ 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,12 +104,11 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
         if (!hasProximitySensor()) {
             getPreferenceScreen().removePreference(mPocketModePref);
         }
-
         mSunlightModePref = (CheckBoxPreference) findPreference(KEY_SUNLIGHT_MODE);
         mSunlightModePref.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.ACTIVE_DISPLAY_SUNLIGHT_MODE, 0) == 1));
         if (!hasLightSensor()) {
-            getPreferenceScreen().removePreference(mSunlightModePref);
+                getPreferenceScreen().removePreference(mSunlightModePref);
         }
 
         PreferenceScreen prefSet = getPreferenceScreen();
@@ -112,6 +118,11 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
                 Settings.System.ACTIVE_DISPLAY_REDISPLAY, 0);
         mRedisplayPref.setValue(String.valueOf(timeout));
         updateRedisplaySummary(timeout);
+
+        mExcludedAppsPref = (AppMultiSelectListPreference) findPreference(KEY_EXCLUDED_APPS);
+        Set<String> excludedApps = getExcludedApps();
+        if (excludedApps != null) mExcludedAppsPref.setValues(excludedApps);
+        mExcludedAppsPref.setOnPreferenceChangeListener(this);
 
         mShowDatePref = (CheckBoxPreference) findPreference(KEY_SHOW_DATE);
         mShowDatePref.setChecked((Settings.System.getInt(getContentResolver(),
@@ -126,10 +137,23 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
                 Settings.System.ACTIVE_DISPLAY_BRIGHTNESS, 100));
         mBrightnessLevel.setOnPreferenceChangeListener(this);
 
-        mExcludedAppsPref = (AppMultiSelectListPreference) findPreference(KEY_EXCLUDED_APPS);
-        Set<String> excludedApps = getExcludedApps();
-        if (excludedApps != null) mExcludedAppsPref.setValues(excludedApps);
-        mExcludedAppsPref.setOnPreferenceChangeListener(this);
+        mDisplayTimeout = (ListPreference) prefSet.findPreference(KEY_TIMEOUT);
+        mDisplayTimeout.setOnPreferenceChangeListener(this);
+        timeout = Settings.System.getLong(getContentResolver(),
+                Settings.System.ACTIVE_DISPLAY_TIMEOUT, 8000L);
+        mDisplayTimeout.setValue(String.valueOf(timeout));
+        updateTimeoutSummary(timeout);
+
+        mProximityThreshold = (ListPreference) prefSet.findPreference(KEY_THRESHOLD);
+        mProximityThreshold.setOnPreferenceChangeListener(this);
+        long threshold = Settings.System.getLong(getContentResolver(),
+                Settings.System.ACTIVE_DISPLAY_THRESHOLD, 5000L);
+        mProximityThreshold.setValue(String.valueOf(threshold));
+        updateThresholdSummary(threshold);
+
+        mTurnOffModePref = (CheckBoxPreference) findPreference(KEY_TURNOFF_MODE);
+        mTurnOffModePref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.ACTIVE_DISPLAY_TURNOFF_MODE, 0) == 1));
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -153,6 +177,14 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
             return true;
         } else if (preference == mExcludedAppsPref) {
             storeExcludedApps((Set<String>) newValue);
+            return true;
+        } else if (preference == mDisplayTimeout) {
+            long timeout = Integer.valueOf((String) newValue);
+            updateTimeoutSummary(timeout);
+            return true;
+        } else if (preference == mProximityThreshold) {
+            long threshold = Integer.valueOf((String) newValue);
+            updateThresholdSummary(threshold);
             return true;
         }
         return false;
@@ -192,6 +224,11 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
                     Settings.System.ACTIVE_DISPLAY_SHOW_AMPM,
                     value ? 1 : 0);
+        } else if (preference == mTurnOffModePref) {
+            value = mTurnOffModePref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.ACTIVE_DISPLAY_TURNOFF_MODE,
+                    value ? 1 : 0);
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
@@ -229,6 +266,24 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
             return null;
 
         return new HashSet<String>(Arrays.asList(excluded.split("\\|")));
+    }
+
+    private void updateTimeoutSummary(long value) {
+        try {
+            mDisplayTimeout.setSummary(mDisplayTimeout.getEntries()[mDisplayTimeout.findIndexOfValue("" + value)]);
+            Settings.System.putLong(getContentResolver(),
+                    Settings.System.ACTIVE_DISPLAY_TIMEOUT, value);
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+    }
+
+    private void updateThresholdSummary(long value) {
+        try {
+            mProximityThreshold.setSummary(mProximityThreshold.getEntries()[mProximityThreshold.findIndexOfValue("" + value)]);
+            Settings.System.putLong(getContentResolver(),
+                    Settings.System.ACTIVE_DISPLAY_THRESHOLD, value);
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
     }
 
     private void storeExcludedApps(Set<String> values) {
