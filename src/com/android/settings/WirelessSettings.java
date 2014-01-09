f91f2c05ffb2177d1ea57e31897dba2541355e80
camcory <camcory1@gmail.com>
2014-01-09 09:28:19 -0500

    [2/3] TeloRadio
    Author: telo46
    
    Some cleanup changed a few strings
    PS3: new icons, action bar switch, java cleanup, dependencies -prime
    PS4: hide on CDMA devices, much derp, such fix, aswesome move, many clean, amaze
    PS5: much fix to please Dave, amaze
    PS6: cleanup
    PS7: such proper fix
    PS8: use constants
    
    Change-Id: Ide33078f9efa8366c97ed328ea94bd81c7573850
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/probam_arrays.xml
    	res/values/probam_strings.xml
    	src/com/android/settings/WirelessSettings.java
    
    Signed-off-by: hany <hany.alsamman@gmail.com>

--- src/com/android/settings/WirelessSettings.java
+++ src/com/android/settings/WirelessSettings.java
@@ -52,6 +52,8 @@ import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.settings.nfc.NfcEnabler;
 
+import com.android.internal.telephony.PhoneConstants;
+
 import java.util.Collection;
 
 public class WirelessSettings extends RestrictedSettingsFragment
@@ -71,6 +73,7 @@ public class WirelessSettings extends RestrictedSettingsFragment
     private static final String KEY_TOGGLE_NSD = "toggle_nsd"; //network service discovery
     private static final String KEY_CELL_BROADCAST_SETTINGS = "cell_broadcast_settings";
     private static final String KEY_SHOW_LTE_OR_FOURGEE = "show_lte_or_fourgee";
+    private static final String KEY_CONNECTION_MANAGER = "connection_manager";
 
     public static final String EXIT_ECM_RESULT = "exit_ecm_result";
     public static final int REQUEST_CODE_EXIT_ECM = 1;
@@ -353,10 +356,16 @@ public class WirelessSettings extends RestrictedSettingsFragment
             mNfcEnabler = null;
         }
 
-        // Remove Mobile Network Settings and Manage Mobile Plan if it's a wifi-only device.
+        // Remove Connection Manager on CDMA devices
+        if (mTm.getPhoneType() == PhoneConstants.PHONE_TYPE_CDMA) {
+            removePreference(KEY_CONNECTION_MANAGER);
+        }
+
+        // Remove Mobile Network Settings, Manage Mobile Plan and Connection Manager if it's a wifi-only device.
         if (isSecondaryUser || Utils.isWifiOnly(getActivity())) {
             removePreference(KEY_MOBILE_NETWORK_SETTINGS);
             removePreference(KEY_MANAGE_MOBILE_PLAN);
+            removePreference(KEY_CONNECTION_MANAGER);
         }
         // Remove Mobile Network Settings and Manage Mobile Plan
         // if config_show_mobile_plan sets false.
