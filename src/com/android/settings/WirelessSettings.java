31027b20f3cef9b35f3ac880b149a84a35823729
Roman Birg <roman@cyngn.com>
2014-03-31 15:58:59 -0700

    Settings: Integrate Voice+ into Settings (1/2)
    Change-Id: I4778443c219cd29a463e9c4dab60783dca251f0f
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/WirelessSettings.java
+++ src/com/android/settings/WirelessSettings.java
@@ -71,12 +71,17 @@ public class WirelessSettings extends RestrictedSettingsFragment
     private static final String KEY_MOBILE_NETWORK_SETTINGS = "mobile_network_settings";
     private static final String KEY_MANAGE_MOBILE_PLAN = "manage_mobile_plan";
     private static final String KEY_SMS_APPLICATION = "sms_application";
+    private static final String KEY_VOICE_PLUS_ACCOUNT = "voice_plus";
     private static final String KEY_TOGGLE_NSD = "toggle_nsd"; //network service discovery
     private static final String KEY_CELL_BROADCAST_SETTINGS = "cell_broadcast_settings";
     private static final String KEY_SHOW_LTE_OR_FOURGEE = "show_lte_or_fourgee";
 
     public static final String EXIT_ECM_RESULT = "exit_ecm_result";
     public static final int REQUEST_CODE_EXIT_ECM = 1;
+
+    private static final String GOOGLE_VOICE_PACKAGE = "com.google.android.apps.googlevoice";
+    private static final ComponentName VOICE_PLUS_SETUP =
+            new ComponentName("org.cyanogenmod.voiceplus", "org.cyanogenmod.voiceplus.VoicePlusSetup");
 
     private AirplaneModeEnabler mAirplaneModeEnabler;
     private CheckBoxPreference mAirplaneModePreference;
@@ -121,6 +126,11 @@ public class WirelessSettings extends RestrictedSettingsFragment
                     Settings.System.SHOW_LTE_OR_FOURGEE,
                     ((CheckBoxPreference) preference).isChecked());
             return true;
+        } else if (preference == findPreference(KEY_VOICE_PLUS_ACCOUNT)) {
+            Intent chooseVoicePlusAccount = new Intent();
+            chooseVoicePlusAccount.setComponent(VOICE_PLUS_SETUP);
+            chooseVoicePlusAccount.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(chooseVoicePlusAccount);
         }
         // Let the intents be launched by the Preference manager
         return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -394,6 +404,11 @@ public class WirelessSettings extends RestrictedSettingsFragment
         // Remove SMS Application if the device does not support SMS
         if (!isSmsSupported()) {
             removePreference(KEY_SMS_APPLICATION);
+        }
+
+        // Remove Voice+ option if Google Voice is not installed
+        if (!isPackageInstalled(GOOGLE_VOICE_PACKAGE)) {
+            removePreference(KEY_VOICE_PLUS_ACCOUNT);
         }
 
         // Remove Airplane Mode settings if it's a stationary device such as a TV.
