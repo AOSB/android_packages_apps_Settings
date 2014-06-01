358c54bb52bce88a344a67ad61edc8fcfd318265
BigBrother1984 <stevewatersy@gmail.com>
2014-06-01 08:56:50 -0300

    [2/2] Add support for Peek app
    Disable built in peek when Peek app is installed. Enable it again on app's unistall.
    
    Change-Id: Icabb32c8322642eca81bccea7e053200a74a9b2b
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- src/com/android/settings/probam/LockscreenNotifications.java
+++ src/com/android/settings/probam/LockscreenNotifications.java
@@ -1,7 +1,12 @@
 package com.android.settings.probam;
 
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.graphics.Color;
@@ -55,6 +60,8 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
     private static final String KEY_GYROSCOPE = "gyroscope_sensor";
     private static final String KEY_PROXIMITY = "proximity_sensor";
 
+    private static final String PEEK_APPLICATION = "com.jedga.peek";
+
     private CheckBoxPreference mNotificationPeek;
     private ListPreference mPeekPickupTimeout;
     private ListPreference mPeekWakeTimeout;
@@ -75,6 +82,23 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
  
     private CheckBoxPreference mGyroscope;
     private CheckBoxPreference mProximity;
+
+    private PackageStatusReceiver mPackageStatusReceiver;
+    private IntentFilter mIntentFilter;
+
+    private boolean isPeekAppInstalled() {
+        return isPackageInstalled(PEEK_APPLICATION);
+    }
+
+    private boolean isPackageInstalled(String packagename) {
+        PackageManager pm = getActivity().getPackageManager();
+        try {
+            pm.getPackageInfo(packagename, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (NameNotFoundException e) {
+           return false;
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -204,13 +228,30 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
             general.removePreference(mShowAlways);
         }
 
+        if (mPackageStatusReceiver == null) {
+            mPackageStatusReceiver = new PackageStatusReceiver();
+        }
+        if (mIntentFilter == null) {
+            mIntentFilter = new IntentFilter();
+            mIntentFilter.addAction(Intent.ACTION_PACKAGE_ADDED);
+            mIntentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        }
+        getActivity().registerReceiver(mPackageStatusReceiver, mIntentFilter);
+
         updateNotificationOptions();
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        getActivity().registerReceiver(mPackageStatusReceiver, mIntentFilter);
         updateState();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(mPackageStatusReceiver);
     }
 
     private void updateState() {
@@ -220,7 +261,8 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
     private void updatePeekCheckbox() {
         boolean enabled = Settings.System.getInt(getContentResolver(),
                 Settings.System.PEEK_STATE, 0) == 1;
-        mNotificationPeek.setChecked(enabled);
+        mNotificationPeek.setChecked(enabled && !isPeekAppInstalled());
+        mNotificationPeek.setEnabled(!isPeekAppInstalled());
     }
 
     @Override
@@ -373,6 +415,18 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
                     Settings.System.PEEK_STATE, 0);
             } else {
                 mNotificationPeek.setEnabled(true);
+            }
+        }
+    }
+
+    public class PackageStatusReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_PACKAGE_ADDED)) {
+                updatePeekCheckbox();
+            } else if(action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
+                updatePeekCheckbox();
             }
         }
     }
